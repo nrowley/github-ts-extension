fe5e42db5f28308bb468db561c278ce149cacec0
nrowley <nelsonononivu2002@gmail.com>
2021-02-09 20:13:57 +0000

    fix commit list

--- src/components/GitItem.tsx
+++ src/components/GitItem.tsx
@@ -1,13 +1,88 @@
+import {
+  AddIcon,
+  ArrowForwardIcon,
+  CheckCircleIcon,
+  CloseIcon,
+} from "@chakra-ui/icons";
+import {
+  Box,
+  Grid,
+  GridItem,
+  List,
+  ListIcon,
+  ListItem,
+  Spacer,
+  Text,
+} from "@chakra-ui/react";
+import { title } from "process";
 import React, { ReactElement } from "react";
 
 interface Props {
-  stuff: string;
+  stuff: any;
 }
 
 export default function GitItem({ stuff }: Props): ReactElement {
+  console.log(stuff);
+
+  const dt = new Date(stuff.created_at).toLocaleTimeString();
+
+  let commits = stuff.payload.commits;
+
+  const combineCommits = (arr): string => {
+    let outStr: string = "";
+    console.log(arr);
+    for (let msg of arr) {
+      outStr += msg.message + "";
+    }
+
+    return outStr.substring(0, outStr.length - 1);
+  };
+
   return (
     <div>
-      <h3>{stuff}</h3>
+      <Box
+        id={stuff.id}
+        maxW="400px"
+        w="100%"
+        mt="5"
+        padding="5"
+        borderWidth="1px"
+        borderRadius="lg"
+        overflow="hidden"
+      >
+        <Box
+          maxW="400px"
+          w="100%"
+          mt="5"
+          textAlign="left"
+          fontWeight="semibold"
+          as="h4"
+        >
+          <Grid templateColumns="repeat(2,1fr)">
+            <GridItem colStart={0}>
+              <Box>
+                <List spacing={3}>
+                  {commits.map((commit) => (
+                    <>
+                      <ListItem>
+                        <ArrowForwardIcon w={3} h={3} mr="2" color="grey" />
+                        {commit.message}
+                      </ListItem>
+                    </>
+                  ))}
+                </List>
+              </Box>
+            </GridItem>
+            <GridItem colEnd={4}>
+              <Box>
+                <Text color="grey" fontSize="sm">
+                  {dt}
+                </Text>
+              </Box>
+            </GridItem>
+          </Grid>
+        </Box>
+      </Box>
     </div>
   );
 }
--- src/components/GitList.tsx
+++ src/components/GitList.tsx
@@ -1,16 +1,28 @@
 import { Heading } from "@chakra-ui/react";
 import axios from "axios";
-import { ReactElement, useState } from "react";
+import { ReactElement, useEffect, useState } from "react";
 import GitItem from "./GitItem";
 
 interface Props {}
 
 export default function GitList({}: Props): ReactElement {
+  const [commits, setCommits] = useState<string[] | null>(null);
+  useEffect(() => {
+    async function getCommits() {
+      const res = await fetch(`https://api.github.com/users/nrowley/events`);
+
+      var data = await res.json();
+      setCommits(data.filter((item) => item.type === "PushEvent"));
+    }
+    getCommits();
+  }, []);
   return (
     <div>
-      {fetch("api.github.com/users/nrowley/events")
-        .then((res) => res.json())
-        .then((data) => console.log(data))}
+      <h3>this is the gitlist</h3>
+      {commits &&
+        commits?.map((commit) => {
+          return <GitItem stuff={commit} />;
+        })}
     </div>
   );
 }
